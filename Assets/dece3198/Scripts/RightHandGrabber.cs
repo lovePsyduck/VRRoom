e53aaf0b7e964639526823f42666d41c3ef0327e
dece3198 <dece3198@naver.com>
2022-10-18 20:43:07 +0900

    file organization

---
+++ Assets/dece3198/Scripts/RightHandGrabber.cs
@@ -0,0 +1,66 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RightHandGrabber : MonoBehaviour
+{
+    public enum GRABBER_MODE
+    {
+        DEFAULT, TOGGLE
+    }
+
+
+    [SerializeField]
+    private GameObject curGrabbableObj;
+    public GRABBER_MODE mode = GRABBER_MODE.DEFAULT;
+
+    private void Update()
+    {
+        if (curGrabbableObj == null)
+        {
+            return;
+        }
+
+        switch (mode)
+        {
+            case GRABBER_MODE.DEFAULT:
+                if (Input.GetButtonDown("Fire2"))
+                {
+                    if (curGrabbableObj.GetComponent<IGrabBeginInteractivable>().IsGrabed == false)
+                    {
+                        curGrabbableObj.GetComponent<IGrabBeginInteractivable>()?.GrabBegin(gameObject);
+                    }
+                    else
+                    {
+                        curGrabbableObj.GetComponent<IGrabBeginInteractivable>()?.GrabEnd(gameObject);
+                    }
+                }
+                break;
+
+
+            case GRABBER_MODE.TOGGLE:
+                if (Input.GetButton("FIre2"))
+                {
+                    curGrabbableObj.GetComponent<IGrabBeginInteractivable>()?.GrabBegin(gameObject);
+                }
+                else
+                {
+                    curGrabbableObj.GetComponent<IGrabBeginInteractivable>()?.GrabEnd(gameObject);
+                }
+                break;
+        }
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        IGrabBeginInteractivable tempGrabbable = other.GetComponent<IGrabBeginInteractivable>();
+        if (tempGrabbable == null) return;
+        curGrabbableObj = other.gameObject;
+    }
+    public void OnTriggerExit(Collider other)
+    {
+        IGrabBeginInteractivable tempGrabbable = other.GetComponent<IGrabBeginInteractivable>();
+        if (tempGrabbable == null) return;
+        curGrabbableObj = null;
+    }
+}
