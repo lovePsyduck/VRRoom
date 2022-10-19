abb2871a3f69f4f3ee9e80a07d4ba308588b04cc
SkyCresent <81556356+SkyCresent@users.noreply.github.com>
2022-10-19 17:46:20 +0900

    이름변경

--- Assets/SkyCresent/Scripts/Rotate.cs
+++ Assets/SkyCresent/Scripts/Rotate.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Test : MonoBehaviour
+public class Rotate : MonoBehaviour
 {
     public float speed = 3f;
     // Update is called once per frame
