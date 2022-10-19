e829305e174d315a5d761ab5dddb8fa34775a4d9
Klistas <fukoyjh@gmail.com>
2022-10-19 16:18:49 +0900

    test

--- Assets/lovePsyduck/Chess_Box.cs
+++ Assets/lovePsyduck/Chess_Box.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ASDF : MonoBehaviour
+public class Chess_Box : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
